8793e7ccfc30444374d2371b04bfbcc9fe27022b
alvarigno <alvaro.emparan@gmail.com>
2019-05-30 22:24:35 -0400

    add order to script for webpack

--- project-GobCl/dist/gobcl.js
+++ project-GobCl/dist/gobcl.js
@@ -87,9 +87,13 @@
 /******/ ({
 
 /***/ "18Fm":
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\n\n@include _assert-ascending($grid-breakpoints, \"$grid-breakpoints\");\n        ^\n      No mixin named -assert-ascending\n      in /Users/alvaroemparan/Documents/GitHub/Base-gobierno-chile/project-GobCl/src/scss/scss/_variables.scss (line 199, column 10)\n    at runLoaders (/Users/alvaroemparan/Documents/GitHub/Base-gobierno-chile/project-GobCl/node_modules/webpack/lib/NormalModule.js:302:20)\n    at /Users/alvaroemparan/Documents/GitHub/Base-gobierno-chile/project-GobCl/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/alvaroemparan/Documents/GitHub/Base-gobierno-chile/project-GobCl/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/alvaroemparan/Documents/GitHub/Base-gobierno-chile/project-GobCl/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.render [as callback] (/Users/alvaroemparan/Documents/GitHub/Base-gobierno-chile/project-GobCl/node_modules/sass-loader/lib/loader.js:52:13)\n    at Object.done [as callback] (/Users/alvaroemparan/Documents/GitHub/Base-gobierno-chile/project-GobCl/node_modules/neo-async/async.js:8067:18)\n    at options.error (/Users/alvaroemparan/Documents/GitHub/Base-gobierno-chile/project-GobCl/node_modules/node-sass/lib/index.js:294:32)");
+exports = module.exports = __webpack_require__("JPst")(false);
+// Module
+exports.push([module.i, "", ""]);
+
+
 
 /***/ }),
 
@@ -197,6 +201,98 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("T4fG");
 /* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_app_scss__WEBPACK_IMPORTED_MODULE_0__);
 
+
+/***/ }),
+
+/***/ "JPst":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
+*/
+// css base code, injected by the css-loader
+module.exports = function (useSourceMap) {
+  var list = []; // return the list of modules as css string
+
+  list.toString = function toString() {
+    return this.map(function (item) {
+      var content = cssWithMappingToString(item, useSourceMap);
+
+      if (item[2]) {
+        return '@media ' + item[2] + '{' + content + '}';
+      } else {
+        return content;
+      }
+    }).join('');
+  }; // import a list of modules into the list
+
+
+  list.i = function (modules, mediaQuery) {
+    if (typeof modules === 'string') {
+      modules = [[null, modules, '']];
+    }
+
+    var alreadyImportedModules = {};
+
+    for (var i = 0; i < this.length; i++) {
+      var id = this[i][0];
+
+      if (id != null) {
+        alreadyImportedModules[id] = true;
+      }
+    }
+
+    for (i = 0; i < modules.length; i++) {
+      var item = modules[i]; // skip already imported module
+      // this implementation is not 100% perfect for weird media query combinations
+      // when a module is imported multiple times with different media queries.
+      // I hope this will never occur (Hey this way we have smaller bundles)
+
+      if (item[0] == null || !alreadyImportedModules[item[0]]) {
+        if (mediaQuery && !item[2]) {
+          item[2] = mediaQuery;
+        } else if (mediaQuery) {
+          item[2] = '(' + item[2] + ') and (' + mediaQuery + ')';
+        }
+
+        list.push(item);
+      }
+    }
+  };
+
+  return list;
+};
+
+function cssWithMappingToString(item, useSourceMap) {
+  var content = item[1] || '';
+  var cssMapping = item[3];
+
+  if (!cssMapping) {
+    return content;
+  }
+
+  if (useSourceMap && typeof btoa === 'function') {
+    var sourceMapping = toComment(cssMapping);
+    var sourceURLs = cssMapping.sources.map(function (source) {
+      return '/*# sourceURL=' + cssMapping.sourceRoot + source + ' */';
+    });
+    return [content].concat(sourceURLs).concat([sourceMapping]).join('\n');
+  }
+
+  return [content].join('\n');
+} // Adapted from convert-source-map (MIT)
+
+
+function toComment(sourceMap) {
+  // eslint-disable-next-line no-undef
+  var base64 = btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap))));
+  var data = 'sourceMappingURL=data:application/json;charset=utf-8;base64,' + base64;
+  return '/*# ' + data + ' */';
+}
 
 /***/ }),
 
