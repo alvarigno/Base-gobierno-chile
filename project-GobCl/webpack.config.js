7182b8ae274cfbe84ce257c30c8bdefdcd2df78d
alvarigno <alvaro.emparan@gmail.com>
2019-06-01 17:55:27 -0400

    Update project

--- project-GobCl/webpack.config.js
+++ project-GobCl/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     //filename: 'bundle.js',
     //filename: 'new-ic[name].[contenthash].js',
-    filename: 'gobcl.js',
+    filename: 'gob.cl.js',
     path: path.resolve(__dirname, 'dist')
     
   },
