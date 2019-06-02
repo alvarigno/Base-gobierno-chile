4b7d5b4fb95383b328265bbb5bda612d54a0c0e5
alvarigno <alvaro.emparan@gmail.com>
2019-06-01 22:06:21 -0400

    add html and project

--- project-GobCl/webpack.config.js
+++ project-GobCl/webpack.config.js
@@ -3,15 +3,15 @@ const webpack = require('webpack');
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   mode: 'none',
   entry: './src/app.js',
   output: {
     //filename: 'bundle.js',
     //filename: 'new-ic[name].[contenthash].js',
-    filename: 'gob.cl.js',
-    path: path.resolve(__dirname, 'dist')
-    
+    filename: 'gob.cl.js'
   },
   watch:true,
   module: {
@@ -66,10 +66,14 @@ module.exports = {
   plugins: [
     new webpack.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      //filename: "new-ic.css",
+      //filename: "[name].css",
+      filename: "gobCL.css",
       chunkFilename: "[id].css"
     }),
+    new CopyPlugin([
+      {from:'src/scss/fonts',to:'fonts'},
+      {from:'src/scss/images',to:'images'}
+  ]), 
       require('autoprefixer')
   ]
 };
